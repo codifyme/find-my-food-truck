42fce5ef43b13f7762783fc4f1746d082801ec3d
Angel Cochachez <acochachez@gmail.com>
2019-11-27 13:16:29 -0800

    added Navigation JS files and adding new dependencies

--- food-truck-finder/src/App.js
+++ food-truck-finder/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Navbar from'./components/navigation/Navbar'
+import {BrowserRouter} from 'react-router-dom'
 
-const MobileNavbar=()=>{
-  return (
-    <div>
-      <MobileNavbar />
-    </div>
-    
-  );
+function App(){
+  return(
+    <BrowserRouter>
+    <Navbar/>
+    </BrowserRouter>
+  )
 }
 
 export default App;
--- food-truck-finder/src/components/navigation/DesktopNavbar.js
+++ food-truck-finder/src/components/navigation/DesktopNavbar.js
@@ -1,26 +1,72 @@
 import React from 'react';
 import styled from 'style-components';
+import { link } from 'react-router-dom';
 
 const MyDesktopNavbar=styled.nav`
     display:flex;
     flex-flow:row nowrap;
-    justify-content:space-everyly;
-    align-item:center;
+    justify-content:space-evenly;
+    align-items:center;
 
-    backgroung:red;
+    background:lightblue;
     color:white;
 
-    height:15;
+    height:15vh;
+
+    .logo{
+        font-size:7vh;
+        font-weight:bold;
+        text-shadow:3px 3px 3px black;
+    }
+    .nav-links{
+        display:flex;
+        flex-flow:row nowrap;
+        justify-content:space-evenly;
+        align-items:center;
+
+        width:35vw;
+        list-style:none;
+    }
+    .link{
+        color:white;
+        font-size:2.5vh;
+        text-decoration:none;
+    }
+`
+
+const MyMobileNavButton=styled.button`
+    background:transparent;
+    height:6vh;
+    width:6vh;
+    border:none;
     `
 
-
-const DesktopNavbar=()=>{
+const DesktopNavbar = () => {
 
         return (
-            <div>
-                DesktopNavbar
-            </div>
-    
+           <MyDesktopNavbar>
+               <div className="logo">Logo</div>
+
+               <ul className="nav-links">
+                   <li>
+                       <link to="/" className="Link">Home</link>
+                   </li>
+                   <li>
+                       <link to="/about" className="Link">About</link>
+                   </li>
+                   <li>
+                       <link to="/map" className="Link">Map</link>
+                   </li>
+                   <li>
+                       <link to="/bookevents" className="Link">Book Events</link>
+                   </li>
+                   <li>
+                       <link to="/recipe" className="Link">recipe</link>
+                   </li>
+
+               </ul>
+               <MyMobileNavButton>Button</MyMobileNavButton>
+           </MyDesktopNavbar>
         );
 };
 
